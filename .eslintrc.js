b3d71bee6f8b1309c87686d866c297d03c89f00b
Maxim Trekusov <trekusovmax@gmail.com>
2021-12-22 13:52:39 +0300

    start

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['plugin:react/recommended', 'standard'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 13,
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {
+    indent: 'off',
+    semi: ['error', 'never'],
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'always',
+        named: 'never',
+        asyncArrow: 'never'
+      }
+    ],
+    'comma-dangle': ['error', 'never']
+  }
+}
